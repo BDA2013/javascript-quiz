27f0b74224e51d3063be3bf3d4b303f24eba52e9
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-05-08 09:13:35 -0400

    added 'quizQuestions', time, score, 'startButton', along with 2 functions

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var time = document.getElementById("time");
+var score = 0;
+var startButton = document.getElementById("start");
+var quizQuestion = [
+    question,
+    choiceA,
+    choiceB,
+    choiceC,
+    choiceD,
+    realAnswer
+];
+
+function startQuiz() {
+    startButton.addEventListener("click", quiz());
+}
+
+function quiz() {
+
+}
